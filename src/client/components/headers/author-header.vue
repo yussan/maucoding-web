427b299ecca68effa6cd552fa523f9645d7e36cc
yussan <main.yussan@gmail.com>
2020-01-01 20:23:52 +0700

    fix #9

---
+++ src/client/components/headers/author-header.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+ .author-header 
+   img(:src="userdata.avatar" :alt="`avatar of ${userdata.username}`")
+   h1 {{ userdata.fullname }}
+   span Writter
+</template>
+<style>
+</style>
+<script>
+
+const props = {
+ userdata: {
+  type: Object
+ }
+}
+
+ exports default {
+  props
+ }
+</script>
