e0e86b939c6482dfadebef2a6482244b2c003175
yussan <main.yussan@gmail.com>
2020-01-17 08:52:54 +0700

    update gitignore

--- dist/config/metainfo.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  title: "Id More Academy - Tech from Engineer Perspective by Id More Team",
-  description: "Here we are not only focused on making tech products. But it also makes technology accessible, affordable and easy for everyone to learn."
-};
