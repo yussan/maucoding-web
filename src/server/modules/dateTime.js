802ee6c7da3408f69880e83f6aac2fb2259ec5aa
yussan <main.yussan@gmail.com>
2020-01-20 14:01:03 +0700

    ready to build

--- src/server/modules/dateTime.js
+++ src/server/modules/dateTime.js
@@ -1,14 +1,16 @@
 /**
  * @description function to convert epoch to custom format date time
- * @param {number} epoch 
- * @param {string} format date time 
+ * @param {number} epoch
+ * @param {string} format date time
  */
-export function epochToFormat(epoch=0, format='') {
+export function epochToFormat(epoch = 0, format = "") {
   const date = new Date(epoch * 1000)
 
-  switch(format) {
-    case 'Y-M-D':
+  switch (format) {
+    case "Y-M-D":
     default:
-      return `${date.getFullYear()}-${String(`0${date.getMonth()}`).slice(-2)}-${String(`0${date.getDate()}`).slice(-2)}`
+      return `${date.getFullYear()}-${String(`0${date.getMonth() + 1}`).slice(
+        -2
+      )}-${String(`0${date.getDate()}`).slice(-2)}`
   }
 }
